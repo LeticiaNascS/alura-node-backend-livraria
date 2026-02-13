d8c482fdc441ab2ed5b627e42804f88ed2a4452a
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-02-13 13:05:49 -0300

    Refactor: Improve error handler to use `erroBase`

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,19 +1,18 @@
- import mongoose from "mongoose";
-import erroBase from '../erros/errosBase.js';
+import mongoose from "mongoose";
+import erroBase from "../erros/errosBase.js";
 import erroValidacao from "../erros/erroValidacao.js";
 import requisicaoIncorreta from "../erros/requisicaoIncorreta.js";
-import naoEncontrado from "../erros/naoEncontrado.js";
 
- function manipuladorDeErros (erro, req, res, next) {
-            if(erro instanceof mongoose.Error.CastError) {
-            new requisicaoIncorreta().enviarResposta(res);    
-            } else if (erro instanceof mongoose.Error.ValidationError) {
-            new erroValidacao(erro).enviarResposta(res);
-            } else if (erro instanceof naoEncontrado) {
-                erro.enviarResposta(res);
-            }else {
-              new erroBase().enviarResposta(res);
-           }
-  };
- 
- export default manipuladorDeErros; 
+function manipuladorDeErros(erro, req, res, next) {
+  if (erro instanceof mongoose.Error.CastError) {
+    new requisicaoIncorreta().enviarResposta(res);
+  } else if (erro instanceof mongoose.Error.ValidationError) {
+    new erroValidacao(erro).enviarResposta(res);
+  } else if (erro instanceof erroBase) {
+    erro.enviarResposta(res);
+  } else {
+    new erroBase().enviarResposta(res);
+  }
+}
+
+export default manipuladorDeErros;
