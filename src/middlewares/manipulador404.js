e456bffc5889b9f61fcb955524c00642ebfa2521
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-02-13 13:05:49 -0300

    Style: Fix file endings and minor whitespace

--- src/middlewares/manipulador404.js
+++ src/middlewares/manipulador404.js
@@ -1,9 +1,8 @@
 import naoEncontrado from "../erros/naoEncontrado.js";
 
 function manipulador404(req, res, next) {
-const erro404 = new naoEncontrado();
-next(erro404);
+  const erro404 = new naoEncontrado();
+  next(erro404);
 }
 
 export default manipulador404;
-    
